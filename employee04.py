eb266fd3c68ebe6324c552429a6a035f909c708e
Sammed Savadatte <sammedsavadatte0@gmail.com>
2025-11-22 17:25:00 +0530

    Update employee04.py

--- employee04.py
+++ employee04.py
@@ -26,7 +26,7 @@ class StreamlitCallbackHandler(BaseCallbackHandler):
 
 # --- PAGE CONFIG ---
 st.set_page_config(page_title=" Chat with Company DB", page_icon="💬", layout="wide")
-st.title("💬  Chat with Company DB")
+st.title("💬  Alpha Assist")
 
 
 # --- LOAD ENV ---
@@ -223,6 +223,7 @@ if user_query:
     if response:
         st.session_state["messages"].append({"role": "assistant", "content": response})
         st_callback_container.markdown(f'<div class="chat-left">{response}</div>', unsafe_allow_html=True)
+
 
 
 
